e7ef6dec41907f8eb235cb222b69e38b8ac7170e
Mytth <Mytth75@gmail.com>
2024-02-08 16:57:05 +0700

    final

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const listOfCharacters = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie']
+const listOfCharacters = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
 class Character {
-    constructor(name, type, health = 100, level = 1, attack, defence) {
-        if (typeof name!== "string" || 
-        listOfCharacters.includes(type) === false) {
-            throw new Error("Invalid input!");
-        }else {
-            this.name = name;
-            this.type = type;
-            this.health = health;
-            this.level = level;
-            this.attack = attack;
-            this.defence = defence;
-        }
+  constructor(name, type, health = 100, level = 1, attack, defence) {
+    if (typeof name !== 'string'
+        || listOfCharacters.includes(type) === false) {
+      throw new Error('Invalid input!');
+    } else {
+      this.name = name;
+      this.type = type;
+      this.health = health;
+      this.level = level;
+      this.attack = attack;
+      this.defence = defence;
     }
+  }
 }
 
 class Bowman extends Character {
-    constructor(name,health,level,attack = 25,defence = 25) {
-      super(name, 'Bowman', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 25, defence = 25) {
+    super(name, 'Bowman', health, level, attack, defence);
+  }
 }
 class Swordsman extends Character {
-    constructor(name,health,level,attack = 40,defence = 10) {
-      super(name, 'Swordsman', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 40, defence = 10) {
+    super(name, 'Swordsman', health, level, attack, defence);
+  }
 }
 class Magician extends Character {
-    constructor(name,health,level,attack = 10,defence = 40) {
-      super(name, 'Magician', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 10, defence = 40) {
+    super(name, 'Magician', health, level, attack, defence);
+  }
 }
 class Undead extends Character {
-    constructor(name,health,level,attack = 25,defence = 25) {
-      super(name, 'Undead', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 25, defence = 25) {
+    super(name, 'Undead', health, level, attack, defence);
+  }
 }
 class Zombie extends Character {
-    constructor(name,health,level,attack = 40,defence = 10) {
-      super(name, 'Zombie', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 40, defence = 10) {
+    super(name, 'Zombie', health, level, attack, defence);
+  }
 }
 class Daemon extends Character {
-    constructor(name,health,level,attack = 10,defence = 40) {
-      super(name, 'Daemon', health,level,attack,defence);
-    }
+  constructor(name, health, level, attack = 10, defence = 40) {
+    super(name, 'Daemon', health, level, attack, defence);
+  }
 }
